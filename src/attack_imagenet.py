5838ae75994aaffde570eecc93ff7e40665f9a98
quqixun <quqixun@gmail.com>
2019-11-24 16:51:15 +0800

    using inception v3 to extract features mean and std of each class data from imagenet

--- src/attack_imagenet.py
+++ src/attack_imagenet.py
@@ -3,41 +3,77 @@ import torch
 import numpy as np
 
 from tqdm import *
-from imageio import imread
-from torchvision import models
+from PIL import Image
+from torchvision import transforms
 from torch.autograd import Variable
+from attack_models import inception_v3
 
 
-imagenet_classes_txt = '../data/imagenet_classes.txt'
-with open(imagenet_classes_txt, 'r') as f:
-    lines = f.readlines()
-imagenet_classes = {}
-for line in lines:
-    class_info = line.strip().split()[:2]
-    imagenet_classes[class_info[0]] = class_info[1]
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+use_cuda = torch.cuda.is_available()
+torch.set_num_threads(6)
+
+
+def create_dir(dir_path):
+    if not os.path.isdir(dir_path):
+        os.makedirs(dir_path)
+
 
 output_dir = '../data/imagenet'
-if not os.path.isdir(output_dir):
-    os.makedirs(output_dir)
+create_dir(output_dir)
 
-model = models.inception_v3(pretrained=True)
+model = inception_v3(pretrained=True)
+if use_cuda:
+    model.cuda()
 model.eval()
 
-imagenet_dir = ''
+preprocess = transforms.Compose([
+    transforms.Resize(299),
+    transforms.CenterCrop(299),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    ),
+])
+
+imagenet_dir = '/data1/pub/dataset/imagenet/train'
 classes_dirs = os.listdir(imagenet_dir)
-for class_dir in tqdm(classes_dirs, ncols=75):
-    class_no = imagenet_classes[class_dir]
-    class_output_dir = os.path.join(output_dir, class_no)
-    samples = os.listdir(imagenet_dir, class_dir)
-    for sample in samples:
+
+for class_name in tqdm(classes_dirs, ncols=75):
+    class_dir = os.path.join(imagenet_dir, class_name)
+    samples = os.listdir(class_dir)
+
+    class_feats, preds = [], []
+    for sample in tqdm(samples, ncols=75):
+        sample_name = sample.split('.')[0].split('_')[1]
         image_path = os.path.join(imagenet_dir, class_dir, sample)
-        image = imread(image_path) / 255
+        image = Image.open(image_path)
+        image = image.convert('RGB')
 
-        image_ = (image - MEAN) / STD
-        image_ = np.transpose(image_, (2, 0, 1))
-        image_ = np.expand_dims(image_, 0)
+        image_tensor = preprocess(image)
+        image_tensor = image_tensor.unsqueeze(0)
+        image_var = Variable(image_tensor)
+        if use_cuda:
+            image_var = image_var.cuda()
 
-        image_var = Variable(torch.Tensor(image_))
-        pred = model(image_var)
+        try:
+            pred, feat = model(image_var)
+            pred = torch.softmax(pred, dim=1)
+            pred = pred.data.cpu().numpy().flatten()
+            class_idx = np.argmax(pred) + 1
+            preds.append(class_idx)
 
+            feat = feat.view(1, -1)
+            feat_np = feat.data.cpu().numpy().flatten()
+            class_feats.append(list(feat_np))
+        except Exception:
+            continue
 
+    preds = np.array(preds)
+    class_no = str(np.argmax(np.bincount(preds)))
+    class_output_dir = os.path.join(output_dir, class_no)
+    create_dir(class_output_dir)
+
+    np.save(os.path.join(class_output_dir, 'mean.npy'), np.mean(class_feats, axis=0))
+    np.save(os.path.join(class_output_dir, 'std.npy'), np.std(class_feats, axis=0))

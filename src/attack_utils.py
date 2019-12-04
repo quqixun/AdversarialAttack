8989e364dad988ae7b6d45e39cd1312761aad832
quqixun <quqixun@gmail.com>
2019-12-04 17:53:09 +0800

    add support for attacking multiple models

--- src/attack_utils.py
+++ src/attack_utils.py
@@ -14,17 +14,21 @@ def compute_score(src_image, adv_image, pred_label, true_label,
     def prosess(image):
         return image.astype(float).flatten()
 
+    if not isinstance(pred_label, list):
+        pred_label = [pred_label]
+
     src_image = prosess(src_image)
     adv_image = prosess(adv_image)
 
     score = 0
     norm_score = norm(adv_image - src_image, ord=norm_ord)
-    if pred_label == true_label:
-        score = 0
-    elif pred_label != true_label and pred_label != target_label:
-        score = 2 * (2 - norm_score / pixel_limit)
-    elif pred_label == target_label:
-        score = 5 * (2 - norm_score / pixel_limit)
-    else:
-        pass
-    return score
+    for pl in pred_label:
+        if pl == true_label:
+            score += 0
+        elif pl != true_label and pl != target_label:
+            score += 2 * (2 - norm_score / pixel_limit)
+        elif pl == target_label:
+            score += 5 * (2 - norm_score / pixel_limit)
+        else:
+            pass
+    return score / len(pred_label)
--- src/attack_whitebox.py
+++ src/attack_whitebox.py
@@ -3,9 +3,7 @@ import numpy as np
 import torch.nn as nn
 
 from PIL import Image
-from imageio import imread
 from torchvision import transforms
-from attack_utils import compute_score
 
 
 class AttackWhiteBox(object):
@@ -15,14 +13,14 @@ class AttackWhiteBox(object):
     # RANGE: [(0 - mean) / std, (1 - mean) / std]
     RANGE = np.array([[-2.1179, 2.2489], [-2.0357, 2.4285], [-1.8044, 2.6400]])
 
-    def __init__(self, model, input_size=299, pixel_limit=32, epsilon=16, alpha=5,
-                 num_iters=50, early_stopping=None, num_threads=1, use_cuda=True):
+    def __init__(self, model, input_size=299, epsilon=16, alpha=5,
+                 num_iters=50, early_stopping=None, num_threads=1,
+                 use_cuda=True):
         torch.set_num_threads(num_threads)
 
         self.alpha = alpha / 255
         self.num_iters = num_iters
         self.epsilon = epsilon / 255
-        self.pixel_limit = pixel_limit
         self.early_stopping = early_stopping
         self.use_cuda = torch.cuda.is_available() and use_cuda
 
@@ -32,25 +30,27 @@ class AttackWhiteBox(object):
             transforms.Normalize(self.MEAN, self.STD),
         ])
 
-        self.model = model.eval()
-        if self.use_cuda:
-            self.model.cuda()
+        if not isinstance(model, list):
+            model = [model]
+        for m in model:
+            m.eval()
+            if self.use_cuda:
+                m.cuda()
+        self.model = model
+
+        # self.model = model.eval()
+        # if self.use_cuda:
+        #     self.model.cuda()
 
         self.ce_loss = nn.CrossEntropyLoss()
         self.l1_loss = nn.L1Loss()
         return
 
-    def __call__(self, image_path, label, target=False, return_score=False):
+    def __call__(self, image_path, label, target=False):
         self.target = target
         adv_image, pred_label = self.forward(image_path, label)
         adv_image_uint8 = adv_image.astype(np.uint8)
-
-        if not return_score:
-            return adv_image_uint8
-        else:
-            score = compute_score(imread(image_path), adv_image, pred_label,
-                                  true_label, target_label, self.pixel_limit)
-            return adv_image_uint8, score
+        return adv_image_uint8, pred_label
 
     def forward(self, image_path, label):
         image = self.preprocess(Image.open(image_path)).unsqueeze(0)
@@ -62,7 +62,7 @@ class AttackWhiteBox(object):
         num_no_improve, best_loss, best_adv_image = 0, None, None
         for i in range(self.num_iters):
             image.requires_grad = True
-            pred = self.model(image)
+            pred = [m(image) for m in self.model]
             loss = self.__loss(pred, label, image, origin)
 
             if best_loss is None or loss.item() < best_loss:
@@ -83,12 +83,15 @@ class AttackWhiteBox(object):
 
     def __loss(self, pred, label, image, origin):
         def compute_ce_loss(p, l):
-            ce_loss = self.ce_loss(pred, label)
+            ce_loss = self.ce_loss(p, l)
             if not self.target:
                 ce_loss = 1 / torch.clamp(ce_loss, min=1e-8)
             return ce_loss
 
-        ce_loss = compute_ce_loss(pred, label)
+        ce_loss = 0
+        for p in pred:
+            ce_loss += compute_ce_loss(p, label)
+        ce_loss /= len(pred)
         return ce_loss + self.l1_loss(image, origin)
 
     def __stop(self, num_no_improve):
@@ -112,10 +115,14 @@ class AttackWhiteBox(object):
         return image_clamp
 
     def __post_process(self, best_adv_image):
-        pred = self.model(best_adv_image)
-        pred = torch.softmax(pred, dim=1)
-        pred = pred.data.cpu().numpy().flatten()
-        pred_label = np.argmax(pred) + 1
+        def pred_adv(model, adv_image):
+            pred = model(adv_image)
+            pred = torch.softmax(pred, dim=1)
+            pred = pred.data.cpu().numpy().flatten()
+            pred_label = np.argmax(pred) + 1
+            return pred_label
+
+        pred_label = [pred_adv(m, best_adv_image) for m in self.model]
 
         adv_image = best_adv_image.squeeze(0).data.cpu().numpy()
         adv_image = np.transpose(adv_image, (1, 2, 0))
@@ -129,40 +136,46 @@ if __name__ == '__main__':
     import pandas as pd
 
     from tqdm import *
-    from imageio import imwrite
+    from imageio import imread, imwrite
     from attack_utils import create_dir
+    from attack_utils import compute_score
     from torchvision.models import inception_v3
-    # from pretrainedmodels import inceptionv4, inceptionresnetv2
+    from pretrainedmodels import inceptionv4, inceptionresnetv2
 
     os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
     images_dir = '../data/images'
     data = pd.read_csv('../data/dev.csv')
-    adv_images_dir = '../data/white_adv_images'
+    adv_images_dir = '../data/white_adv_images/inception-3'
     create_dir(adv_images_dir)
 
+    models = [inception_v3(pretrained=True),
+              inceptionv4(pretrained='imagenet'),
+              inceptionresnetv2(pretrained='imagenet')]
+
     attack = AttackWhiteBox(
-        model=inception_v3(pretrained=True),
-        # model=inceptionv4(pretrained='imagenet'),
-        # model=inceptionresnetv2(pretrained='imagenet'),
-        input_size=299, pixel_limit=32,
-        epsilon=16, alpha=1, num_iters=50,
-        early_stopping=None, num_threads=2
+        model=models, input_size=299,
+        epsilon=16, alpha=1, num_iters=100,
+        early_stopping=10, num_threads=2
     )
 
+    print('-' * 75)
+    print('Attacking - inception family')
     scores = []
-    for i, sample in tqdm(data.iterrows(), total=len(data), ncols=80):
+    for i, sample in tqdm(data.iterrows(), total=len(data), ncols=75):
         true_label = sample['TrueLabel']
         target_label = sample['TargetClass']
 
         image_path = os.path.join(images_dir, sample['ImageId'])
-        adv_image, score = attack(image_path, true_label, target=False, return_score=True)
-        # adv_image, score = attack(image_path, target_label, target=True, return_score=True)
+        adv_image, pred_label = attack(image_path, target_label, target=True)
+
+        score = compute_score(imread(image_path), adv_image, pred_label,
+                              true_label, target_label, pixel_limit=32)
         scores.append(score)
 
         adv_image_path = os.path.join(adv_images_dir, sample['ImageId'])
         imwrite(adv_image_path, adv_image)
-        break
 
     mean_score = np.mean(scores)
-    print('Attack Score: {:.6f}'.format(mean_score))
+    print('\nAttack Score: {:.6f}'.format(mean_score))
+    print('-' * 75)

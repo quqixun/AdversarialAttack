7b756513368a7ca640e92035ba0673cf9fca1844
Qixun Qu <quqixun@gmail.com>
2020-02-04 22:36:22 +0800

    implement simba

--- attack/blackbox.py
+++ attack/blackbox.py
@@ -14,23 +14,22 @@ class BlackBoxAttack(object):
     # RANGE: [(0 - mean) / std, (1 - mean) / std]
     RANGE = np.array([[-2.1179, 2.2489], [-2.0357, 2.4285], [-1.8044, 2.6400]])
 
-    def __init__(self, model, input_size=224, epsilon=16, alpha=5,
+    def __init__(self, model, input_size=224, epsilon=16,
                  num_iters=50, early_stopping=None, use_cuda=False):
         '''__INIT__
             reference:
-            Kurakin A, Goodfellow I, Bengio S.
-            Adversarial examples in the physical world[J].
-            arXiv preprint arXiv:1607.02533, 2016.
+            Guo C, Gardner J R, You Y, et al.
+            Simple black-box adversarial attacks[J].
+            arXiv preprint arXiv:1905.07121, 2019.
+
             model: model instance or list of model instances
             input_size: int, size of input tentor to model
             epsilon: int, limit on the perturbation size
-            alpha: int, step size for gradient-based attack
             num_iters: int, number of iterations
             early_stopping: int ot None, attack will not stop unless loss stops improving
             use_cuda: bool, True or False, whether to use GPU
         '''
 
-        self.alpha = alpha / 255
         self.num_iters = num_iters
         self.epsilon = epsilon / 255
         self.early_stopping = early_stopping
@@ -51,9 +50,6 @@ class BlackBoxAttack(object):
             if self.use_cuda:
                 m.cuda()
         self.model = model
-
-        self.l1_loss = nn.SmoothL1Loss()
-        self.ce_loss = nn.CrossEntropyLoss()
         return
 
     def __call__(self, image_path, label, target=False):
@@ -66,121 +62,79 @@ class BlackBoxAttack(object):
         '''
 
         self.target = target
-        src_image = Image.open(image_path)
-        adv_image, pred_label = self.forward(src_image, label)
-        return adv_image, pred_label
+        src_image = np.array(Image.open(image_path)) / 255.0
+        print(src_image.shape)
+        # adv_image, pred_label = self.forward(src_image, label)
+        self.forward(src_image, label)
+        # return adv_image, pred_label
+        return
 
     def forward(self, src_image, label):
-        # n_dims = x.view(1, -1).size(1)
-        # perm = torch.randperm(n_dims)
-        # # last_prob = get_probs(model, x, y)
-        # last_prob = self.__predict(model, x, y)
-        # for i in range(num_iters):
-        #     diff = torch.zeros(n_dims)
-        #     diff[perm[i]] = epsilon
-        #     left_prob = get_probs(model, (x - diff.view(x.size())).clamp(0, 1), y)
-        #     if left_prob < last_prob:
-        #         x = (x - diff.view(x.size())).clamp(0, 1)
-        #         last_prob = left_prob
-        #     else:
-        #         right_prob = get_probs(model, (x + diff.view(x.size())).clamp(0, 1), y)
-        #         if right_prob < last_prob:
-        #             x = (x + diff.view(x.size())).clamp(0, 1)
-        #             last_prob = right_prob
+        image = torch.Tensor(src_image)
+        image = image.unsqueeze(0)
+        n_dims = image.view(1, -1).size(1)
+        perm = torch.randperm(n_dims)
+        last_prob = self.__predict(image, label)
+
+        for i in range(self.num_iters):
+            diff = torch.zeros(n_dims)
+            diff[perm[i]] = self.epsilon
+
+            left_image = image - diff.view(image.size()).clamp(0, 1)
+            left_prob = self.__predict(left_image, label)
+            if left_prob < last_prob:
+                image = left_image
+                last_prob = left_prob
+            else:
+                right_image = image + diff.view(image.size()).clamp(0, 1)
+                right_prob = self.__predict(right_image, label)
+                if right_prob < last_prob:
+                    image = right_image
+                    last_prob = right_prob
+            print(last_prob)
         return
 
-    def __predict(self, image, model):
-        # image_norm = self.__norm(image)
-        # pred = model(image_norm)
-        # return pred
-        return
+    def __predict(self, image, label):
+        def get_prob(model, image_norm):
+            pred = model(image_norm)
+            probs = torch.softmax(pred, dim=1)
+            probs = probs.data.cpu().detach().numpy().flatten()
+            return probs[label]
+
+        image_norm = self.__norm(image)
+        if self.use_cuda:
+            image_norm = image_norm.cuda()
+        probs = [get_prob(model, image_norm) for model in self.model]
+        return np.mean(probs)
 
     def __norm(self, image):
-        # imgae_norm = self.preprocess(image)
-        # return image_norm
-        return
+        image_cp = image.clone()
+        image_cp = image_cp.squeeze(0).permute(2, 0, 1)
+        image_cp = transforms.ToPILImage()(image_cp)
+        image_norm = self.preprocess(image_cp)
+        image_norm = image_norm.unsqueeze(0)
+        return image_norm
 
-    # def forward(self, src_image, label):
-    #     image = self.preprocess(src_image).unsqueeze(0)
-    #     origin = image.clone().detach()
-    #     label = torch.LongTensor([label])
-    #     if self.use_cuda:
-    #         image, origin, label = image.cuda(), origin.cuda(), label.cuda()
+    # def __clamp(self, image):
+    #     image_clamp = []
+    #     for i in range(image.size()[1]):
+    #         cmin, cmax = self.RANGE[i]
+    #         image_i = torch.clamp(image[0][i], min=cmin, max=cmax)
+    #         image_clamp.append(image_i)
+    #     image_clamp = torch.stack(image_clamp, dim=0).unsqueeze(0)
+    #     return image_clamp
 
-    #     num_no_improve, best_iter = 0, None
-    #     best_loss, best_adv_image = None, None
-    #     for i in range(self.num_iters):
-    #         image.requires_grad = True
-    #         pred = [m(image) for m in self.model]
-    #         loss = self.__loss(pred, label, image, origin)
+    # def __post_process(self, best_adv_image):
+    #     def pred_adv(model):
+    #         pred = model(best_adv_image)
+    #         pred = torch.softmax(pred, dim=1)
+    #         pred = pred.data.cpu().detach().numpy().flatten()
+    #         pred_label = np.argmax(pred)
+    #         return pred_label
 
-    #         iter_msg = '[Step:{:0=3d}/{:0=3d}]-[Loss:{:12.6f}]'
-    #         print(iter_msg.format(i + 1, self.num_iters, loss.item()), end='\r')
-
-    #         if best_loss is None or loss.item() < best_loss:
-    #             best_adv_image = image.clone()
-    #             best_loss = loss.item()
-    #             num_no_improve = 0
-    #             best_iter = i
-    #         else:
-    #             num_no_improve += 1
-    #         if self.__stop(num_no_improve):
-    #             stop_msg = '\n[Early stopped]-[Step:{:0=3d}]-[Loss:{:.6f}]'
-    #             print(stop_msg.format(best_iter + 1, best_loss))
-    #             break
-
-    #         image = self.__PGD(loss, image, origin)
-    #         image_clamp = self.__clamp(image)
-    #         image = image_clamp.detach()
-
-    #     adv_image, pred_label = self.__post_process(best_adv_image)
+    #     pred_label = [pred_adv(m) for m in self.model]
+    #     adv_image = best_adv_image.squeeze(0).data.cpu().detach().numpy()
+    #     adv_image = np.transpose(adv_image, (1, 2, 0))
+    #     adv_image = adv_image * self.STD + self.MEAN
+    #     adv_image = np.round(adv_image * 255.0).astype(np.uint8)
     #     return adv_image, pred_label
-
-    # def __loss(self, pred, label, image, origin):
-    #     def compute_ce_loss(p, l):
-    #         if self.target:
-    #             ce_loss = self.ce_loss(p, l)
-    #         else:
-    #             ce_loss = 1 / torch.clamp(self.ce_loss(p, l), min=1e-8)
-    #         return ce_loss
-
-    #     ce_loss = 0
-    #     for p in pred:
-    #         ce_loss += compute_ce_loss(p, label)
-    #     ce_loss /= len(pred)
-    #     return ce_loss + self.l1_loss(image, origin)
-
-    # def __stop(self, num_no_improve):
-    #     return (self.early_stopping is not None) and \
-    #         (num_no_improve == self.early_stopping)
-
-    # def __PGD(self, loss, image, origin):
-    #     grad = torch.autograd.grad(loss, image, retain_graph=False)[0]
-    #     perturbation = image - self.alpha * grad.sign() - origin
-    #     perturbation = torch.clamp(perturbation, min=-self.epsilon, max=self.epsilon)
-    #     image = origin - perturbation
-    #     return image
-
-    def __clamp(self, image):
-        image_clamp = []
-        for i in range(image.size()[1]):
-            cmin, cmax = self.RANGE[i]
-            image_i = torch.clamp(image[0][i], min=cmin, max=cmax)
-            image_clamp.append(image_i)
-        image_clamp = torch.stack(image_clamp, dim=0).unsqueeze(0)
-        return image_clamp
-
-    def __post_process(self, best_adv_image):
-        def pred_adv(model):
-            pred = model(best_adv_image)
-            pred = torch.softmax(pred, dim=1)
-            pred = pred.data.cpu().detach().numpy().flatten()
-            pred_label = np.argmax(pred)
-            return pred_label
-
-        pred_label = [pred_adv(m) for m in self.model]
-        adv_image = best_adv_image.squeeze(0).data.cpu().detach().numpy()
-        adv_image = np.transpose(adv_image, (1, 2, 0))
-        adv_image = adv_image * self.STD + self.MEAN
-        adv_image = np.round(adv_image * 255.0).astype(np.uint8)
-        return adv_image, pred_label
